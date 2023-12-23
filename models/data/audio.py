d0799b33108cba640c4bda1af891ea3d48c72106
Hazarskiy <tovarish.isaev@gmail.com>
2023-12-23 23:05:05 +0300

    Init 0.002nw

--- models/data/audio.py
+++ models/data/audio.py
@@ -7,6 +7,7 @@ class Audio(Model):
     owner = ForeignKeyField(Post, backref='audios', index=True)
     url = TextField()
     file_id = TextField(null=True)
+    file_path = CharField(null=True)
     artist = CharField()
     title = CharField()
     class Meta:
